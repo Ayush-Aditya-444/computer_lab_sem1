3ae0ba1046eab184bc485b8e1e2b4e736d8051c5
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-04-23 09:12:50 +0530

    Add files via upload

---
+++ Chapter 1 Q6..cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+	int a=1189, b=841;
+	float c, d ,e, f, g, h, i, j, k, l, m, n, o, p, q, r;
+	printf("Dimensions Of A0 are 1189*841");
+	c=a/2;
+	d=b/2;
+	e=c/2;
+	f=d/2;
+	g=e/2;
+	h=f/2;
+	i=g/2;
+	j=h/2;
+	k=i/2;
+	l=j/2;
+	m=k/2;
+	n=l/2;
+	o=m/2;
+	p=n/2;
+	q=o/2;
+	r=p/2;
+	printf("\nDimensions Of A1= %f*%f", c, d);
+		printf("\nDimensions Of A2= %f*%f", e, f);
+			printf("\nDimensions Of A3= %f*%f", g, h);
+				printf("\nDimensions Of A4= %f*%f", i, j);
+					printf("\nDimensions Of A5= %f*%f", k, l);
+						printf("\nDimensions Of A6= %f*%f", m, n);
+							printf("\nDimensions Of A7= %f*%f", o, p);
+								printf("\nDimensions Of A8= %f*%f", q, r);
+}
