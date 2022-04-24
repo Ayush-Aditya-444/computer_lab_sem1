5ef199890ac98bfcda23f846f6463e5472e6c831
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-04-24 18:17:18 +0530

    Add files via upload

---
+++ Chapter 2 Q8..cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int a, b, c, d, e, f, g;
+	printf("Enter Your First Number :");
+	scanf("%d", &a);
+	printf("Enter Your Second Number :");
+	scanf("%d", &b);
+	c=a;
+	d=b;
+	printf("%d%d", d, c);
+}
